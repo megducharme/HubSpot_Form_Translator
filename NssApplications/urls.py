4c87b6ab5b0739d90f05fc5672d50e85f29b21d0
Meg Ducharme <ducharme.meg@gmail.com>
2018-03-13 13:36:29 -0500

    general styling and css grid attempt

--- NssApplications/urls.py
+++ NssApplications/urls.py
@@ -1,7 +1,8 @@
 from django.urls import include, path
 from django.contrib import admin
+from django.conf.urls import url
 
 urlpatterns = [
-    path('applications/', include('applications.urls')),
+    url(r'^', include('applications.urls')),
     path('admin/', admin.site.urls),
 ]
