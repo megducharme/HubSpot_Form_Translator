6544638f24f5535af55466de01a080d74fe0e499
Meg Ducharme <ducharme.meg@gmail.com>
2018-03-13 18:22:14 -0500

    updated with admin info at top of application

--- applications/static/applications/main.js
+++ applications/static/applications/main.js
@@ -11,3 +11,33 @@ var yyyy = today.getFullYear();
 
 today = yyyy+'-'+mm+'-'+dd;
 document.getElementById("date").defaultValue = today
+
+
+let TCD = ["TCD", "TCD signed", "TCD sent"]
+for (var i=0;i<TCD.length;i++){
+    $('<option/>').val(TCD[i]).html(TCD[i]).appendTo('#TCD');
+ }
+
+let cohort = ["Cohort", "C26", "C27", "C28", "C29", "C30", "C31", "E7", "E8", "E9", "E10"]
+for (var i=0;i<cohort.length;i++){
+    $('<option/>').val(cohort[i]).html(cohort[i]).appendTo('#cohort');
+ }
+
+
+let tuition = ["Tuition", "Opp", "Reg"]
+for (var i=0;i<tuition.length;i++){
+    $('<option/>').val(tuition[i]).html(tuition[i]).appendTo('#tuition');
+ }
+
+let local = ["Local/Remote", "Local", "Remote"]
+for (var i=0;i<local.length;i++){
+    $('<option/>').val(local[i]).html(local[i]).appendTo('#local');
+ }
+
+
+$('#date').html(mm + "/" + dd + "/" + yyyy)
+
+let time = ["Time", "2:30pm", "3:30pm", "4:30pm", "8:00am"]
+for (var i=0;i<time.length;i++){
+    $('<option/>').val(time[i]).html(time[i]).appendTo('#time');
+ }
