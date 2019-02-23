8919f908360bbc112cd9272ec1f52479adbb1321
Steve Brownlee <chortlehoort@gmail.com>
2019-02-23 00:22:03 -0600

    Config whitenoise to cache eternally

--- NssApplications/settings.py
+++ NssApplications/settings.py
@@ -127,3 +127,4 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join("/", BASE_DIR, 'static_assets')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
